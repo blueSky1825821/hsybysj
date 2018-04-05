13a3eb836db0ae09c8f00a13b0ce400ef453c230
wangmin <1664637427@qq.com>
2018-04-05 08:48:32 +0800

    整理

--- src/main/java/com/adempiere/bysj/controller/InvoiceController.java
+++ src/main/java/com/adempiere/bysj/controller/InvoiceController.java
@@ -1,19 +1,14 @@
 package com.adempiere.bysj.controller;
 
 import com.adempiere.bysj.domain.InvoiceDO;
-import com.adempiere.bysj.domain.SalesOrderDO;
-import com.adempiere.bysj.domain.User;
 import com.adempiere.bysj.service.InvoiceService;
-import com.adempiere.bysj.service.SalesOrderService;
 import com.adempiere.bysj.tools.HandleSession;
-import com.alibaba.fastjson.JSON;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
